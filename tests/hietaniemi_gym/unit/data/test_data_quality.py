14cf6850025015a3a5552c30a27bf3873ea483a8
Oleksandr Balabanov <Balabanovsasha@gmail.com>
2024-02-13 06:47:15 +0100

    env update

--- tests/hietaniemi_gym/unit/data/test_data_quality.py
+++ tests/hietaniemi_gym/unit/data/test_data_quality.py
@@ -3,15 +3,17 @@ import os
 from dotenv import load_dotenv
 from src.projects.hietaniemi_gym.data.data_processing.data_loader import load_gym_data
 import pytest
+from pathlib import Path
 
 @pytest.fixture(scope="module")
 def hietaniemi_gym_data_df():
     # load env
     app_env = 'development'
-    env_file = f"config/.env.{app_env}"
+    env_file = Path(f"config/.env.{app_env})"
     load_dotenv(dotenv_path=env_file)
     # get data path
     gym_data_path = os.getenv('GYM_DATA_PATH')
+    gym_data_path = Path(os.getenv('GYM_DATA_PATH'))
     return load_gym_data(gym_data_path)
 
 def test_row_count(hietaniemi_gym_data_df):
